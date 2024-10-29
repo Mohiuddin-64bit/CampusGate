06d2da4b0a83a1dbe58c32efe9d08ace1a743f94
Mohiuddin <mohiuddin.niddu@gmail.com>
2024-10-29 16:01:19 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <button className='btn btn-neutral'>
+        Hello, click me
+      </button>
+    </>
+  )
+}
+
+export default App
