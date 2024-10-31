b5e9e2cd8c734b477c24543e9f1d32857b143c5a
Mohiuddin <mohiuddin.niddu@gmail.com>
2024-10-31 19:03:51 +0600

    chore: add firebase credential to env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8nsoIXxatjiJ7N-5P7ZN0lQEKDbEN-T4",
-  authDomain: "collegegate-39ee4.firebaseapp.com",
-  projectId: "collegegate-39ee4",
-  storageBucket: "collegegate-39ee4.appspot.com",
-  messagingSenderId: "579036163550",
-  appId: "1:579036163550:web:9981cb848560656673814f",
-  measurementId: "G-0BE77XMPWJ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
-
 
 export { auth, analytics };
